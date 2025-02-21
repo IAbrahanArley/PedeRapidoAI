450a1994ad23fc57b8ae09b95a0ea3950f1b3eea
IAbrahanarley <abrahanarley96@gmail.com>
2025-02-21 16:44:56 -0300

    Feat: navegacao para pedidos

--- src/app/[slug]/menu/[productId]/components/products-header.tsx
+++ src/app/[slug]/menu/[productId]/components/products-header.tsx
@@ -2,7 +2,7 @@
 import { Product } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useParams,useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 
@@ -11,9 +11,12 @@ interface ProductHeaderProps {
 }
 const ProductHeader = ({product}: ProductHeaderProps) => {
     const router = useRouter();
-    
+    const { slug } = useParams<{ slug: string }>();
     const handleBack = () => {
         router.back();
+    };
+    const handleOrderClick = () => {
+        router.push(`/${slug}/orders`);
     };
     return ( 
         <div className="relative w-full h-[300px]">
@@ -30,6 +33,7 @@ const ProductHeader = ({product}: ProductHeaderProps) => {
         variant={"secondary"}
         className="absolute right-4 top-4 z-50 rounded-full"
         size="icon"
+        onClick={handleOrderClick}
       >
         <ScrollTextIcon />
       </Button>
